12ebd551d4cffa3e0e5625a6d96ffee1027af519
UtasiB <utasibalazs@turr.hu>
2023-06-01 10:27:04 +0200

    Alap feltöltése

---
+++ Első projekt/Assets/scripts/followPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(-30, 0, 10);
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
